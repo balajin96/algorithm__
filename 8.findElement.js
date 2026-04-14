42b516d07af674f01d8c6e2db0f0f8a6ff53f1d6
balajin96 <balajinatarajan18011996@gmail.com>
2026-04-14 07:16:52 +0530

    findElement

---
+++ 8.findElement.js
@@ -0,0 +1,20 @@
+let arr = [1, 2, 3, 4, 5];
+
+function findElement(arr, target) {
+    // for (let i = 0; i <= arr.length; i++) {
+    //     if (arr[i] === target) {
+    //         return true
+    //     }
+    // }
+    // return false
+
+    // for (let x of arr) {
+    //     if (x === target) {
+    //         return true
+    //     }
+    // } return false
+}
+// above code similar to includes()
+// console.log(arr.includes(6));
+console.log(findElement(arr, 5))
+
